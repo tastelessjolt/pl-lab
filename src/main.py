41e866c74a044a506e2e190beeeb23b9ffb63186
Harshith Goka <harshith9399@gmail.com>
2018-01-26 15:02:09 +0530

    add argparse

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 from lex import *
 from yacc import *
 import sys
+import argparse
+
+VERSION = '0.1.0'
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -11,15 +14,21 @@ def process(data):
     yacc.yacc()
     yacc.parse(data)
 
+def buildParser():
+    parser = argparse.ArgumentParser(description='APL Compiler ver ' + VERSION)
+    parser.add_argument('input_file', type=str,
+                        help='program to compile')
+    return parser
+
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        eprint("Usage: " + sys.argv[0] + " <filename>")
-        sys.exit()
+
+    parser = buildParser()
+    args = parser.parse_args()
 
     lexer = APLLexer()
     lexer.build()
 
-    filename = sys.argv[1]
+    filename = args.input_file
     data = open(filename, 'r').read()
 
     lexer.test_t(data)
