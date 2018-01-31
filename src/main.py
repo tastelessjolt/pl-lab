33ce60b1cbbb882a7057430c20ff853e388e8e3f
Harshith Goka <harshith9399@gmail.com>
2018-01-31 16:22:58 +0530

    added lex/yacc only flags

--- src/main.py
+++ src/main.py
@@ -15,8 +15,8 @@ def buildArgParser():
     parser = argparse.ArgumentParser(description='APL Compiler ver ' + VERSION)
     parser.add_argument('input_file', type=str,
                         help='program to compile')
-    parser.add_argument('-l', help='generate only lex output')
-    parser.add_argument('-y', help='generate only yacc output')
+    parser.add_argument('-l', '--lex', help='generate lex output', action='store_true')
+    parser.add_argument('-y', '--yacc', help='generate yacc output', action='store_true')
     return parser
 
 if __name__ == '__main__':
@@ -32,12 +32,13 @@ if __name__ == '__main__':
 
     filename = args.input_file
     data = open(filename, 'r').read()
-    if False:
-        lexer.test_t(data)
-        sys.exit()
 
-    stats = parser.parse(data)
-    if stats is not None:
-        print(stats.t)
-    else: 
-        eprint("Exited due to above errors")
+    if args.lex:
+        lexer.test_t(data)
+
+    if args.yacc or not (args.lex or args.yacc):
+        stats = parser.parse(data)
+        if stats is not None:
+            print(stats.t)
+        else: 
+            eprint("Exited due to above errors")
