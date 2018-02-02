db2cb2a91e7d18bf845bac0ac6e9a30c592e4004
Akash Trehan <akash.trehan123@gmail.com>
2018-02-03 02:03:31 +0530

    Change lex test class from TestClass to TestLex

--- src/lex/tests/test_lex.py
+++ src/lex/tests/test_lex.py
@@ -2,7 +2,7 @@ import sys
 import os
 from lex import APLLexer
 
-class TestClass(object):
+class TestLex(object):
     tests_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../testcases/')
     tests_out = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'outputs/')
     lex_tests_dir = os.path.join(tests_dir, 'lex')
@@ -11,23 +11,23 @@ class TestClass(object):
         lexer = APLLexer()
         lexer.build()
 
-        for file in os.listdir(TestClass.tests_dir):
-            filepath = os.path.join(TestClass.tests_dir, file)
+        for file in os.listdir(TestLex.tests_dir):
+            filepath = os.path.join(TestLex.tests_dir, file)
             if os.path.isfile(filepath):
                 f = open(filepath)
                 l = lexer.scan(f.read())
 
-                output = open(os.path.join(TestClass.tests_out, file)).read()
+                output = open(os.path.join(TestLex.tests_out, file)).read()
                 assert output.strip() == (capsys.readouterr().err + l.__str__().strip())
 
     def test_lex_only(self, capsys):
         lexer = APLLexer()
         lexer.build()
 
-        for file in os.listdir(TestClass.lex_tests_dir):
-            filepath = os.path.join(TestClass.lex_tests_dir, file)
+        for file in os.listdir(TestLex.lex_tests_dir):
+            filepath = os.path.join(TestLex.lex_tests_dir, file)
             if os.path.isfile(filepath):
                 f = open(filepath)
                 l = lexer.scan(f.read())
-                output = open(os.path.join(TestClass.tests_out, file)).read()
+                output = open(os.path.join(TestLex.tests_out, file)).read()
                 assert output.strip() == (capsys.readouterr().err + l.__str__().strip())
