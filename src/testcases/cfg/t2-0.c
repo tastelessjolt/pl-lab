f8b934789132e4f02295f404824c0ad0b8ceea25
Harshith Goka <harshith9399@gmail.com>
2018-03-03 22:04:30 +0530

    added output cfg; t20;

--- src/testcases/cfg/t2-0.c
+++ src/testcases/cfg/t2-0.c
@@ -1,6 +1,5 @@
 void main()
 {
-   int *****f3;
    int *f2,*********f8,****s3;
    int *f1;
    int i3;
@@ -14,30 +13,29 @@ void main()
 
    if(*f1 != 0)
    {
-    *f3 = 4;
-    *f4 = 4;
+    *f2 = 4;
+    *f2 = 4;
      if(*f2 != 0){
         *iftmp0 = 1;
-        *b=4;
+        *f2=4;
      }
      else {
         *iftmp0 = 0;
         if(*f2 != 1) {
           *iftmp0 = 1;
-          *b=4;
+          *f2=4;
         }
         else {
           *iftmp0 = 1;
-          *b=4;
+          *f2=4;
         }
-        *f4 = 4;
+        *f2 = 4;
      }
    }
     else {
       *iftmp0 = 0;
-      *m = 4;
+      *f2 = 4;
     }
-    *d = b;
      **i1 = *iftmp0;
 
 }
