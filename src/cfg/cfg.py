f84bf1f7d411bdab73652b49b1571b136f6001c6
Akash Trehan <akash.trehan123@gmail.com>
2018-03-03 21:59:04 +0530

    Added awesome testcase

--- src/cfg/cfg.py
+++ src/cfg/cfg.py
@@ -1,5 +1,8 @@
+
+from st import *
+
 class CFG(object):
-    class BasicBlock(object):
+    class Block(object):
         def __init__(self, astlist, blocknum):
             self.astlist = astlist
             self.blocknum = blocknum
@@ -12,5 +15,34 @@ class CFG(object):
         self.blocks = []
         self.traverse_ast()
 
-    def traverse_ast(self):
+    def traverse_if(self, if_ast, blocknum):
         raise NotImplementedError
+
+    def traverse_while(self, while_ast, blocknum):
+        raise NotImplementedError
+
+    def traverse_ast(self):
+        block_number = 0
+        function_list = self.ast.function_list
+        main_func = [func for func in function_list if func.fname == 'main'][0]
+        stlist = main_func.stlist
+        block_list = []
+
+        j = 0
+
+        while(j < len(stlist)):
+            while(not (isinstance(stlist[j], IfStatement) or isinstance(stlist[j], WhileStatement))):
+                block_list.append(stlist[j])
+                j += 1
+            self.blocks.append(Block(block_list, block_number))
+            block_number += 1
+            block_list = []
+            self.blocks.append(Block([stlist[j]], block_number))
+            block_number += 1
+
+            if isinstance(stlist[j], IfStatement):
+                traverse_if(stlist[j], block_number)
+            else:
+                traverse_while(stlist[j], block_number)
+
+            j += 1
---
+++ src/testcases/cfg/t2-0.c
@@ -0,0 +1,43 @@
+void main()
+{
+   int *****f3;
+   int *f2,*********f8,****s3;
+   int *f1;
+   int i3;
+   int ***i2;
+   int **i1;
+   int *iftmp0;
+
+   i2 = &i1;
+   *f2 = 4;
+
+
+   if(*f1 != 0)
+   {
+    *f3 = 4;
+    *f4 = 4;
+     if(*f2 != 0){
+        *iftmp0 = 1;
+        *b=4;
+     }
+     else {
+        *iftmp0 = 0;
+        if(*f2 != 1) {
+          *iftmp0 = 1;
+          *b=4;
+        }
+        else {
+          *iftmp0 = 1;
+          *b=4;
+        }
+        *f4 = 4;
+     }
+   }
+    else {
+      *iftmp0 = 0;
+      *m = 4;
+    }
+    *d = b;
+     **i1 = *iftmp0;
+
+}
