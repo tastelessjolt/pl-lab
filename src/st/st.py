e43c1ef13083e51b5ea729ed41e1ddb6758a2216
Akash Trehan <akash.trehan123@gmail.com>
2018-03-07 16:09:23 +0530

    AST construction and printing complete

--- src/st/st.py
+++ src/st/st.py
@@ -5,9 +5,12 @@ class AST(object):
 class Program(AST):
     def __init__(self, funclist):
         self.funclist = funclist
+    
+    def __str__(self):
+        return '\n'.join([str(func) for func in self.funclist])
 
-    def __str__(self, depth = 0):
-        return 'Program {\n' + inc_tabsize('\n'.join([func.__str__() for func in self.funclist])) + '\n}'
+    def __repr__(self):
+        return 'Program {\n%s\n}' % inc_tabsize('\n'.join([repr(func) for func in self.funclist]))
 
 class Func(AST):
     def __init__(self, rtype, fname, params, stlist):
@@ -15,9 +18,19 @@ class Func(AST):
         self.fname = fname
         self.params = params
         self.stlist = stlist
+    
+    def __str__(self):
+        s = ''
+        for st in self.stlist:
+            if isinstance(st, list):
+                s += '\n'.join([str(i) for i in st]) + "\n"
+            elif not isinstance(st, Declaration):
+                s += '\n' + str(st) + "\n"
+        return s
 
-    def __str__(self, depth = 0):
-        return 'Func(' + ', '.join([self.fname, str(self.params), str(self.rtype)]) + ') {\n' + inc_tabsize('\n'.join([str(st) for st in self.stlist])) + '\n}'
+    def __repr__(self):
+        return 'Func(%s) {\n%s\n}' % (', '.join([self.fname, str(self.params), str(self.rtype)]),
+                                        inc_tabsize('\n'.join([repr(st) for st in self.stlist])))
 
 class IfStatement(AST):
     def __init__(self, operator, condition, stlist1, stlist2=[]):
@@ -26,13 +39,34 @@ class IfStatement(AST):
         self.stlist1 = stlist1
         self.stlist2 = stlist2
 
-    def __str__(self, depth = 0):
-        tmp = 'If(' + self.condition.__repr__() + ') '
+    def __str__(self):
+        s = 'IF(\n%s\n\t,\n' % inc_tabsize(str(self.condition))
+
+        # Statements in the if block
         if len(self.stlist1) != 0:
-            tmp += '{\n' + inc_tabsize('\n'.join([st.__str__() for st in self.stlist1])) + '\n}'
+            for st in self.stlist1:
+                if isinstance(st, list):
+                    s += '%s\n' % inc_tabsize('\n'.join([str(i) for i in st]))
+                else:
+                    s += '%s\n' % inc_tabsize(str(st))
+        # Statements in else block
         if len(self.stlist2) != 0:
-            tmp += '\nelse {\n' + inc_tabsize('\n'.join([st.__str__() for st in self.stlist2])) + '\n}'
-        return tmp
+            for st in self.stlist2:
+                if isinstance(st, list):
+                    s += '%s\n' % inc_tabsize('\n'.join([str(i) for i in st]))
+                else:
+                    s += '%s\n' % inc_tabsize(str(st))
+
+        s += '\n)'
+        return s
+
+    def __repr__(self):
+        s = 'If(' + str(self.condition) + ') '
+        if len(self.stlist1) != 0:
+            s += '{\n' + inc_tabsize('\n'.join([repr(st) for st in self.stlist1])) + '\n}'
+        if len(self.stlist2) != 0:
+            s += '\nelse {\n' + inc_tabsize('\n'.join([repr(st) for st in self.stlist2])) + '\n}'
+        return s
 
 class WhileStatement(AST):
     def __init__(self, operator, condition, stlist):
@@ -40,11 +74,25 @@ class WhileStatement(AST):
         self.condition = condition
         self.stlist = stlist
 
-    def __str__(self, depth = 0):
-        tmp = 'While(' + self.condition.__repr__() + ') '
+    def __str__(self):
+        s = 'WHILE(\n%s\n\t,\n' % inc_tabsize(str(self.condition))
+
+        # Statements inside while
         if len(self.stlist) != 0:
-            tmp += '{\n' + inc_tabsize('\n'.join([st.__str__() for st in self.stlist])) + '\n}'
-        return tmp
+            for st in self.stlist:
+                if isinstance(st, list):
+                    s += '%s\n' % inc_tabsize('\n'.join([str(i) for i in st]))
+                else:
+                    s += '%s\n' % inc_tabsize(str(st))
+        
+        s += '\n)'
+        return s
+
+    def __repr__(self):
+        s = 'While(' + repr(self.condition) + ') '
+        if len(self.stlist) != 0:
+            s += '{\n' + inc_tabsize('\n'.join([repr(st) for st in self.stlist])) + '\n}'
+        return s
 
 class ScopeBlock(AST):
     '''
@@ -53,16 +101,20 @@ class ScopeBlock(AST):
     def __init__(self, stlist):
         self.stlist = stlist
 
-    def __str__(self, depth=0):
-        raise NotImplementedError
+    def __str__(self):
+        return '\n'.join([str(st) for st in self.stlist])
+
+    def __repr__(self):
+        return '\n'.join([repr(st) for st in self.stlist])
 
 class Declaration(AST):
     # varlist is list of Symbol ASTs
     def __init__(self, symlist):
         self.symlist = symlist
 
-    def __str__(self, depth = 0):
-        return repr(self)
+    def __str__(self):
+        # AST does not have declarations
+        return ''
 
     def __repr__(self):
         return ','.join ([ repr(sym) for sym in self.symlist ])
@@ -73,8 +125,8 @@ class Symbol(AST):
         self.label = label
         self.datatype = datatype
 
-    def __str__(self, depth=0):
-        return "\t" * depth + "VAR(%s)" % self.label
+    def __str__(self):
+        return "VAR(%s)" % self.label
 
     def __repr__(self):
         return str((self.label, self.datatype))
@@ -89,18 +141,12 @@ class BinOp(AST):
         self.operand1 = operand1
         self.operand2 = operand2
 
-    def __str__(self, depth=0):
-        tab = "\t" * depth
-        s = tab + "%s\n" % self.operator
-        s += tab + "(\n"
-        s += "%s\n" % self.operand1.__str__(depth=depth + 1)
-        s += tab + "\t,\n"
-        s += "%s\n" % self.operand2.__str__(depth=depth + 1)
-        s += tab + ")"
+    def __str__(self):
+        s = "%s\n(\n%s\n\t,\n%s\n)" % (self.operator, inc_tabsize(str(self.operand1)), inc_tabsize(str(self.operand2)))
         return s
     
     def __repr__(self):
-        return repr(self.operator) + '(' + repr(self.operand1) + ', ' + repr(self.operand2) + ')'
+        return "%s(%s,%s)" % (repr(self.operator), repr(self.operand1), repr(self.operand2))
 
 
 class UnaryOp(AST):
@@ -108,24 +154,20 @@ class UnaryOp(AST):
         self.operator = operator
         self.operand = operand
 
-    def __str__(self, depth=0):
-        tab = "\t" * depth
-        s = tab + "%s\n" % self.operator
-        s += tab + "(\n"
-        s += "%s\n" % self.operand.__str__(depth=depth + 1)
-        s += tab + ")"
+    def __str__(self):
+        s = "%s\n(\n\t%s\n)" % (self.operator, str(self.operand))
         return s
 
     def __repr__(self):
-        return repr(self.operator) + '(' + repr(self.operand) + ')'
+        return "%s(%s)" % (repr(self.operator), repr(self.operand))
 
 
 class Var(AST):    
     def __init__(self, label):
         self.label = label
 
-    def __str__(self, depth=0):
-        return "\t" * depth + "VAR(%s)" % self.label
+    def __str__(self):
+        return "VAR(%s)" % self.label
 
     def __repr__(self):
         return self.label
@@ -134,8 +176,8 @@ class Num(AST):
     def __init__(self, val):
         self.val = val
 
-    def __str__(self, depth=0):
-        return "\t" * depth + "CONST(%d)" % self.val
+    def __str__(self):
+        return "CONST(%d)" % self.val
 
     def __repr__(self):
         return str(self.val)
