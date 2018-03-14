0bc3e2b196f16558d2e03c32edfaee4a22af5620
Akash Trehan <akash.trehan123@gmail.com>
2018-03-14 15:21:53 +0530

    Add outputs for cfg; Reduce redundancy in test_st; .ast .cfg no more ignored

--- src/st/tests/test_st.py
+++ src/st/tests/test_st.py
@@ -9,48 +9,35 @@ class TestAST(object):
     tests_out = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'outputs/')
     ast_tests_dir = os.path.join(tests_dir, 'st')
 
+    def execute_test(self, file, filepath, capsys):
+        lexer = APLLexer()
+        lexer.build()
+
+        parser = APLYacc(output=YaccOutput.AST)
+        parser.build(lexer)
+
+        f = open(filepath)
+        ast = parser.parse(f.read())
+
+        ast_str = ''
+        if ast:
+            ast_str += 'Successfully Parsed\n'
+            ast_str += str(ast)
+
+        output = open(os.path.join(TestAST.tests_out, file)).read()
+        errout = capsys.readouterr().err
+        print(file)
+        assert output.strip() == (errout + ast_str).strip()
+
     def test_common(self, capsys):
         for file in os.listdir(TestAST.tests_dir):
             filepath = os.path.join(TestAST.tests_dir, file)
             if os.path.isfile(filepath) and file.endswith(".c"):
-                lexer = APLLexer()
-                lexer.build()
-
-                parser = APLYacc(output = YaccOutput.AST)
-                parser.build(lexer)
-
-                f = open(filepath)
-                ast = parser.parse(f.read())
-
-                ast_str = ''
-                if ast:
-                    ast_str += 'Successfully Parsed\n'
-                    ast_str += str(ast)
-
-                output = open(os.path.join(TestAST.tests_out, file)).read()
-                errout = capsys.readouterr().err
-                print(file)
-                assert output.strip() == (errout + ast_str).strip()
+                self.execute_test(file, filepath, capsys)
 
     def test_ast_only(self, capsys):
         for file in os.listdir(TestAST.ast_tests_dir):
             filepath = os.path.join(TestAST.ast_tests_dir, file)
             if os.path.isfile(filepath) and file.endswith(".c"):
-                lexer = APLLexer()
-                lexer.build()
-
-                parser = APLYacc(output = YaccOutput.AST)
-                parser.build(lexer)
-
-                f = open(filepath)
-                ast = parser.parse(f.read())
-
-                ast_str = ''
-                if ast:
-                    ast_str += 'Successfully Parsed\n'
-                    ast_str += str(ast)
-                    
-                output = open(os.path.join(TestAST.tests_out, file)).read()
-                errout = capsys.readouterr().err
-                print(file)
-                assert output.strip() == (errout + ast_str).strip()
+                self.execute_test(file, filepath, capsys)
+                
