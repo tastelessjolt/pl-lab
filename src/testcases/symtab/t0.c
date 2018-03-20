8b1794365f262815ac82ed69abaa175f62963f38
Harshith Goka <harshith9399@gmail.com>
2018-03-21 02:08:47 +0530

    added symtab testcase

---
+++ src/testcases/symtab/t0.c
@@ -0,0 +1,39 @@
+int *n, *p;
+int *q, *global;
+
+int ***fun(int *);
+// int ***fun(int *);
+// int ***fun();
+int *fun1(int **b, int *a);
+float *fun2(int *);
+float *fun3(int***, int* a, int* b);
+
+void main () {
+    int *a, **b; 
+    *b = a + (-fun1 (b, a));
+}
+
+int ***fun(int *a)
+{
+    int *an, *qas;
+    if (*a == 2)
+    {
+        int *q;
+        *q = 3;
+    }
+    else
+    {
+        int *p;
+    }
+}
+
+int *fun1(int **b, int *a)
+{
+}
+
+float *fun3(int ***ad, int *adfs, int *bf)
+{
+    float *fad, af;
+    *fad = 3.4;
+    return fad;
+}
